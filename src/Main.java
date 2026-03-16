299006670ae284fe9663dd1c5e5112627ea7beb2
Szanto Marius Cristian <crxrupe@gmail.com>
2026-03-16 10:33:44 +0200

    Am adaugat citire fisier

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,61 @@
-void main() {
-Student s=new Student(333,231,"Popescu","Marcel");
-System.out.println(s.ParsareDate());
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.util.Collections;
+
+//Mai trebuie implementate hashmap,treemap etc.
+//Lab3 Tema:sortare in functie de grupa si dupa nume (daca doi elevi au acelasi nume si aceeasi grupa sa ii sorteze dupa nume)
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        Student s=new Student(333,231,"Popescu","Marcel",true);
+        Student s1=new Student(393,211,"Ioan","Arbore",false);
+        Student s2=new Student(323,221,"Travolta","John",true);
+
+        ArrayList<Student> slista=new ArrayList<>();
+        slista.add(s);
+        slista.add(s1);
+        slista.add(s2);
+
+
+
+        for(Student x:slista)
+        {
+            System.out.println(x);
+        }
+
+        System.out.println("Prezenta: ");
+        for(Student x:slista)
+        {
+            String numeCautat=sc.nextLine();
+            x.VerificarePrezenta(numeCautat);
+        }
+
+        ArrayList<Student> sflista=new ArrayList<>();
+        try {
+            Scanner scf=new Scanner(new File("C:\\ULBS\\Paradigme de Programare\\Proiect2\\src\\Input"));
+            while(scf.hasNextLine())
+            {
+                Integer nrMatricol = scf.nextInt();
+                Integer formatie = scf.nextInt();
+                String nume= scf.next();
+                String prenume= scf.next();
+                boolean Prezent = scf.nextBoolean();
+                if (sc.nextLine().contains("mere")) System.out.println("Am gasit mere!");
+                sflista.add(new Student(nrMatricol,formatie,nume,prenume,Prezent));
+            }
+            Collections.sort(sflista);
+            for(Student studenti:sflista)
+            {
+                System.out.println(studenti);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Fisierul nu a fost gasit: " + e.getMessage());
+        }
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,22 @@
-public class Student {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Student implements Comparable<Student> {
     Integer nrMatricol;
     Integer formatie;
     String nume;
     String prenume;
+    boolean Prezent;
 
-    Student(Integer nrMatricol, Integer formatie, String nume, String prenume)
+    Student(Integer nrMatricol, Integer formatie, String nume, String prenume,boolean Prezent)
     {
         this.nrMatricol=nrMatricol;
         this.formatie=formatie;
         this.nume=nume;
         this.prenume=prenume;
+        this.Prezent=Prezent;
     }
     @Override
     public String toString()
@@ -20,5 +27,26 @@ public class Student {
     String ParsareDate()
     {
         return toString();
+    }
+
+    void VerificarePrezenta(String numedeCautat)
+    {
+       if(Prezent==true && this.nume.equalsIgnoreCase(numedeCautat)) System.out.println("Studentul cu numele: " + nume + " " + "este prezent!");
+       else System.out.println("Numele nu a fost gasit sau studentul cu numele: " + " " + numedeCautat + " este absent!");
+
+    }
+
+    @Override
+    public int compareTo(Student other) {
+
+        if (this.formatie != other.formatie) {
+            return Integer.compare(this.formatie, other.formatie);
+        }
+
+        if (!this.nume.equals(other.nume)) {
+            return this.nume.compareTo(other.nume);
+        }
+
+        return this.prenume.compareTo(other.prenume);
     }
 }
