5ed5322c33f5ce2827eaa6337b9f8241ca8831a9
Szanto Marius Cristian <crxrupe@gmail.com>
2026-03-23 11:05:06 +0200

    Am adaugat cautare dupa note HASHMAP

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
@@ -8,6 +9,7 @@ import java.util.Collections;
 
 //Mai trebuie implementate hashmap,treemap etc.
 //Lab3 Tema:sortare in functie de grupa si dupa nume (daca doi elevi au acelasi nume si aceeasi grupa sa ii sorteze dupa nume)
+//Lab4 Prima parte: Citim nume si dam numere asociate numelor. Citim fisier in hashmap, apoi dam ca input un student si sa ii returneze nota. Generam metoda de .equals si hashcode
 
 public class Main
 {
@@ -23,20 +25,22 @@ public class Main
         slista.add(s1);
         slista.add(s2);
 
-
+        //HashMap<Integer,String> StudentiNumere=new HashMap<>();
+        HashMap<Integer, String> StudentiNote=new HashMap<>();
+        HashMap<String, Integer> StudentiMatricol=new HashMap<>();
 
         for(Student x:slista)
         {
             System.out.println(x);
         }
-
+/*      //Am scos prezenta ca e destul de annoying, cere input mereu de la tastatura.
         System.out.println("Prezenta: ");
         for(Student x:slista)
         {
             String numeCautat=sc.nextLine();
             x.VerificarePrezenta(numeCautat);
         }
-
+*/
         ArrayList<Student> sflista=new ArrayList<>();
         try {
             Scanner scf=new Scanner(new File("C:\\ULBS\\Paradigme de Programare\\Proiect2\\src\\Input"));
@@ -49,6 +53,7 @@ public class Main
                 boolean Prezent = scf.nextBoolean();
                 if (sc.nextLine().contains("mere")) System.out.println("Am gasit mere!");
                 sflista.add(new Student(nrMatricol,formatie,nume,prenume,Prezent));
+                StudentiMatricol.put(nume,nrMatricol);
             }
             Collections.sort(sflista);
             for(Student studenti:sflista) {
@@ -64,6 +69,32 @@ public class Main
             }
 
             output.close();
+            /*          //Prima parte introducere hashmap
+            Integer i=1;
+            for(Student studenti:sflista)
+            {
+                StudentiNumere.put(i,studenti.prenume);
+                i++;
+            }
+
+
+            System.out.println("Cauta in Hashmap dupa numar: ");
+            System.out.println(StudentiNumere.get(sc.nextInt()));
+            System.out.println(StudentiNumere);
+*/
+            Scanner scf2=new Scanner(new File("C:\\ULBS\\Paradigme de Programare\\Proiect2\\src\\Input2"));
+            while(scf2.hasNextLine())
+            {
+                Integer nrMatricol=scf2.nextInt();
+                Integer Nota= scf2.nextInt();
+                StudentiNote.put(nrMatricol,Nota.toString());
+            }
+            System.out.println(StudentiNote);
+            System.out.println(StudentiMatricol);
+            String numeDeCautatNota= "";
+            System.out.print("Cauta un nume ca sa aflii nota: ");
+            numeDeCautatNota=sc.nextLine();
+            System.out.println(StudentiNote.get(StudentiMatricol.get(numeDeCautatNota)));
             System.out.println("Date scrise in fisier cu succes!");
         } catch (IOException e) {
             System.out.println("Fisierul nu a fost gasit: " + e.getMessage());
